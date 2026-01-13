b5e1075c69ce5478bb53564cf4f3a7eb617622f0
dazthecorgi <daz_the_corgi@proton.me>
2026-01-13 17:15:18 +0000

    Fork node/p2p/blossumsub

--- simtest/proxy/main.go
+++ simtest/proxy/main.go
@@ -11,8 +11,7 @@ import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"source.quilibrium.com/quilibrium/monorepo/config"
-	"source.quilibrium.com/quilibrium/monorepo/node/p2p"
-	p2ptypes "source.quilibrium.com/quilibrium/monorepo/types/p2p"
+	"source.quilibrium.com/quilibrium/monorepo/simtest/proxy/p2p"
 )
 
 var configDirectory = flag.String(
@@ -26,24 +25,6 @@ var network = flag.Uint(
 	0,
 	"sets the active network for the node (mainnet = 0, primary testnet = 1)",
 )
-
-type ProxyNode struct {
-	pubSub p2ptypes.PubSub
-}
-
-func NewProxyNode(
-	pubSub p2ptypes.PubSub,
-) (*ProxyNode, error) {
-	return &ProxyNode{
-		pubSub: pubSub,
-	}, nil
-}
-
-func (pn *ProxyNode) Stop() {
-	go func() {
-		pn.pubSub.Close()
-	}()
-}
 
 func main() {
 	flag.Parse()
@@ -71,15 +52,11 @@ func main() {
 	logger.Info("Starting DHT-only node...")
 
 	blossomSub := p2p.NewBlossomSub(nodeConfig.P2P, nodeConfig.Engine, logger, 0, p2p.ConfigDir(*configDirectory))
-	proxyNode, err := NewProxyNode(blossomSub)
-	if err != nil {
-		logger.Fatal("failed to start proxy node", zap.Error(err))
-	}
 
 	logger.Info("DHT node running. Press Ctrl+C to stop.")
 
 	<-done
 
 	logger.Info("Shutting down DHT node...")
-	proxyNode.Stop()
+	blossomSub.Close()
 }
